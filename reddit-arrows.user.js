866483419376b25ace209dfd9748e56b2d54fae4
Rumo <44436342+ItsRumo@users.noreply.github.com>
2024-10-23 15:22:49 +0200

    Update reddit-arrows.user.js

--- reddit-arrows.user.js
+++ reddit-arrows.user.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name         Reddit Arrow Keys
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Allow arrowkeys to control reddit gallery
+// @author       Rumo
+// @match        http*://*.reddit.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
 (() => {
   let keymap = {
     ArrowLeft: "prevButton",
